4fe951bd7ed2f9861a586f0b0f2b81f8272ae34a
kakanishi <kenneth.t.miura@gmail.com>
2020-03-25 12:19:35 -0400

    turns out the spotify autoRating is kinda trash

--- auto_rate.py
+++ auto_rate.py
@@ -3,6 +3,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import json
 import dbus
 import time
+import csv
+from collections import defaultdict
 
 # Note: The wait time probably varies depending on hardware, and how good spotify's implementation is. Also depends on Dbus. 
 def parse_ratings(playlist_length):
@@ -10,7 +12,7 @@ def parse_ratings(playlist_length):
     bus = dbus.SessionBus()
     spotify_mpris_id = 'org.mpris.MediaPlayer2.spotify'
     player = bus.get_object(spotify_mpris_id, '/org/mpris/MediaPlayer2')
-    song_ratings = {}
+    song_ratings = defaultdict(float)
     for i in range(playlist_length):
         metadata = player.Get('org.mpris.MediaPlayer2.Player',
                               'Metadata',
@@ -21,12 +23,8 @@ def parse_ratings(playlist_length):
         print(f'title: {title}, rating: {rating}')
         song_ratings[title] = rating
         time.sleep(wait_time_in_secs)
-    print("pre-sort")
-    print(song_ratings)
-    sorted_song_ratings = sorted(song_ratings.items(), key=lambda x: x[1])
+    sorted_song_ratings = dict(sorted(song_ratings.items(), key=lambda x: x[1]))
 
-    print("post sort")
-    print(sorted_song_ratings)
     return sorted_song_ratings
 
 
@@ -34,11 +32,17 @@ def main():
     # Assumes that Credentials are set up as environment variables
     spotify = spotipy.Spotify(
         client_credentials_manager=SpotifyClientCredentials())
-    playlist_id = 'spotify:playlist:2P5zEQaeWuplHLRK845geh'
-    playlist_length = spotify.playlist(playlist_id)['tracks']['total']
+    playlist_id = 'spotify:playlist:6OY8LffwEltyoG4rb0kkVr'
+    playlist = spotify.playlist(playlist_id)
+    playlist_length = playlist['tracks']['total']
     print(f"playlist length: {playlist_length}")
-    parse_ratings(playlist_length)
-
-
+    playlist_name = playlist['name']
+    print(f"playlist_name: {playlist_name}")
+    ratings = parse_ratings(playlist_length)
+    print(len(ratings))
+    with open('rating.csv', mode='w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        print(ratings.items())
+        writer.writerows(ratings.items())
 if __name__ == '__main__':
     main()
